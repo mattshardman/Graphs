a62671299829124ca47c2618152fcef96fed168f
mattshardman <mattshardman@gmail.com>
2019-11-06 20:11:01 +0000

    update ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,13 +1,13 @@
-class Stack:
+class Queue:
     def __init__(self):
         self.stack = []
 
-    def push(self, int):
+    def enqueue(self, int):
         self.stack.append(int)
 
-    def pop(self):
+    def dequeue(self):
         if len(self.stack) > 0:
-            return self.stack.pop(0)
+            return self.stack.pop(-1)
         else: 
             return None
 
@@ -26,25 +26,35 @@ class Graph:
         else:
             raise KeyError("Key doesn't exist")
 
-    def dfs(self, node):
-        s = Stack()
-        s.push(node)
+    def bfs(self, starting_node):
+        q = Queue()
+        q.enqueue([starting_node])
 
-        visited = set()
+        max_path_length = 1
 
-        while len(s.stack) > 0:
-            v = s.pop()
-        
-            if not v in visited:
-                visited.add(v)
+        earliest_ancestor = -1
 
-                for next_vertex in self.vertices[v]:
-                    s.push(next_vertex)
+        while len(q.stack) > 0:
+            # dequeue the path
+            path = q.dequeue()
+            # get the last vert
+            vert = path[-1]
+            # if path is longer or equal and the value is smaller, or if the path is longer
+            if (len(path) >= max_path_length and vert < earliest_ancestor) or (len(path) > max_path_length):
+                # set the earliest ancestor to the vert
+                earliest_ancestor = vert
+                # set the max path length to the len of the path
+                max_path_length = len(path)
+            # loop over each neighbor in the graphs vertices at index of vert
+            for neighbor in self.vertices[vert]:
+                # make a copy of the path
+                path_copy = list(path)
+                # append neighbor to the coppied path
+                path_copy.append(neighbor)
+                # then enqueue the copied path
+                q.enqueue(path_copy)
 
-        if v == node:
-            return -1
-        else:
-            return v
+        return earliest_ancestor
 
 
 def earliest_ancestor(ancestors, starting_node):
@@ -54,5 +64,9 @@ def earliest_ancestor(ancestors, starting_node):
         graph.add_vertice(child)
         graph.add_edge(parent, child)
 
-    return graph.dfs(starting_node)
+    return graph.bfs(starting_node)
 
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+earliest_ancestor(test_ancestors, 1)
